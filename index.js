9d4666ee36e05256493826cbf1c0fd420d33d80a
hare <always.swimmin@gmail.com>
2017-06-22 04:32:03 +0000

    xd?

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var id = "";
+var is_playing = true;
+var time_stamp = 0.0;
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/chat.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('id', function(msg){
+    id = msg;
+    console.log('play at ' + time_stamp);
+    socket.broadcast.emit('id', id);
+  });
+  socket.on('play', function(msg){
+    time_stamp = msg;
+    is_playing = true;
+    console.log('play at ' + time_stamp);
+    socket.broadcast.emit('play', time_stamp);
+  });
+  socket.on('pause', function(msg){
+    time_stamp = msg;
+    is_playing = false;
+    console.log('pause at ' + time_stamp);
+    socket.broadcast.emit('pause', time_stamp);
+  });
+  socket.on('move', function(msg){
+    time_stamp = msg;
+    console.log('move to ' + time_stamp);
+    socket.broadcast.emit('move', time_stamp);
   });
 });
 
