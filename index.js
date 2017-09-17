6b5ad181a238f8f428c79ad4b5d4e934f7617b96
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2017-09-17 07:52:57 -0400

    :D

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	// socket.emit('id', id);
+	var room = "";
 	socket.on('onLoad', function(){
 		socket.emit('id', id);
 		if(isPlaying) {
@@ -21,17 +22,22 @@ io.on('connection', function(socket){
 			socket.emit('pause', timeStamp);
 		}
 	});
+	socket.on('join', function(msg){
+		socket.leave(room);
+		room = msg;
+		socket.join(msg);
+	});
 	socket.on('id', function(msg){
 		id = msg;
 		console.log('play at ' + timeStamp);
-		socket.broadcast.emit('id', id);
+		socket.broadcast.to(room).emit('id', id);
 	});
 	socket.on('play', function(msg){
 		if(!isPlaying || Math.abs(timeStamp - msg) > threshold) {
 			timeStamp = msg;
 			isPlaying = true;
 			console.log('play at ' + timeStamp);
-			socket.broadcast.emit('play', timeStamp);
+			socket.broadcast.to(room).emit('play', timeStamp);
 		}
 	});
 	socket.on('pause', function(msg){
@@ -39,14 +45,14 @@ io.on('connection', function(socket){
 			timeStamp = msg;
 			isPlaying = false;
 			console.log('pause at ' + timeStamp);
-			socket.broadcast.emit('pause', timeStamp);
+			socket.broadcast.to(room).emit('pause', timeStamp);
 		}
 	});
 	socket.on('move', function(msg){
 		if(Math.abs(timeStamp - msg) > threshold) {
 			timeStamp = msg;
 			console.log('move to ' + timeStamp);
-			socket.broadcast.emit('move', timeStamp);
+			socket.broadcast.to(room).emit('move', timeStamp);
 		}
 	});
 });
