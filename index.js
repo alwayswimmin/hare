d0f65e893dec790a391302f302f4998cebdf1958
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2017-09-17 08:25:46 -0400

    ave maria

--- index.js
+++ index.js
@@ -2,57 +2,54 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var id = "";
-var isPlaying = true;
-var timeStamp = 0.0;
-var threshold = 0.5;
-
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+var videoId = new Array();
+var state = new Array();
+var timeStamp = new Array();
+
 io.on('connection', function(socket){
-	// socket.emit('id', id);
 	var room = "";
-	socket.on('onLoad', function(){
-		socket.emit('id', id);
-		if(isPlaying) {
-			socket.emit('play', timeStamp);
-		} else {
-			socket.emit('pause', timeStamp);
-		}
-	});
+	var threshold = 0.5;
 	socket.on('join', function(msg){
 		socket.leave(room);
 		room = msg;
 		socket.join(msg);
+		socket.emit('id', videoId[room]);
+		if(state[room] == 1) {
+			socket.emit('play', timeStamp[room]);
+		} else {
+			socket.emit('pause', timeStamp[room]);
+		}
 	});
 	socket.on('id', function(msg){
-		id = msg;
-		console.log('play at ' + timeStamp);
-		socket.broadcast.to(room).emit('id', id);
+		videoId[room] = msg;
+		console.log('play at ' + timeStamp[room]);
+		socket.broadcast.to(room).emit('id', videoId[room]);
 	});
 	socket.on('play', function(msg){
-		if(!isPlaying || Math.abs(timeStamp - msg) > threshold) {
-			timeStamp = msg;
-			isPlaying = true;
-			console.log('play at ' + timeStamp);
-			socket.broadcast.to(room).emit('play', timeStamp);
+		if(state[room] != 1 || Math.abs(timeStamp[room] - msg) > threshold) {
+			timeStamp[room] = msg;
+			state[room] = 1;
+			console.log('play at ' + timeStamp[room]);
+			socket.broadcast.to(room).emit('play', timeStamp[room]);
 		}
 	});
 	socket.on('pause', function(msg){
-		if(isPlaying || Math.abs(timeStamp - msg) > threshold) {
-			timeStamp = msg;
-			isPlaying = false;
-			console.log('pause at ' + timeStamp);
-			socket.broadcast.to(room).emit('pause', timeStamp);
+		if(state[room] != 2 || Math.abs(timeStamp[room] - msg) > threshold) {
+			timeStamp[room] = msg;
+			state[room] = 2;
+			console.log('pause at ' + timeStamp[room]);
+			socket.broadcast.to(room).emit('pause', timeStamp[room]);
 		}
 	});
 	socket.on('move', function(msg){
-		if(Math.abs(timeStamp - msg) > threshold) {
-			timeStamp = msg;
-			console.log('move to ' + timeStamp);
-			socket.broadcast.to(room).emit('move', timeStamp);
+		if(Math.abs(timeStamp[room] - msg) > threshold) {
+			timeStamp[room] = msg;
+			console.log('move to ' + timeStamp[room]);
+			socket.broadcast.to(room).emit('move', timeStamp[room]);
 		}
 	});
 });
