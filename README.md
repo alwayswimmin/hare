f4e01afb46f8cc9476d44595cc25f7fc3707145e
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2017-09-16 20:27:49 -0400

    bugfix! and indents

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-DigitalOcean Guides
----
-- [Initial Server Setup with Ubuntu 16.04](https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04)
-- [LAMP Python](https://www.digitalocean.com/community/tutorials/how-to-set-up-an-apache-mysql-and-python-lamp-server-without-frameworks-on-ubuntu-14-04)
-
-Our Droplet
----
-ip: `104.236.32.255`
-
-username: `root`
-password: `rootpassword`
-
-username: `hare`
-password: `harepassword`
-
-mysql:
-username: `root`
-password: `mysqlpassword`
+Hare
+===
+Watch videos together
--- index.js
+++ index.js
@@ -3,46 +3,54 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var id = "";
-var is_playing = true;
-var time_stamp = 0.0;
+var isPlaying = true;
+var timeStamp = 0.0;
+var threshold = 0.5;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('onLoad', function(){
-      socket.emit('id', id);
-      if(is_playing) {
-          socket.emit('play', time_stamp);
-      } else {
-          socket.emit('pause', time_stamp);
-      }
-  });
-  socket.on('id', function(msg){
-    id = msg;
-    console.log('play at ' + time_stamp);
-    socket.broadcast.emit('id', id);
-  });
-  socket.on('play', function(msg){
-    time_stamp = msg;
-    is_playing = true;
-    console.log('play at ' + time_stamp);
-    socket.broadcast.emit('play', time_stamp);
-  });
-  socket.on('pause', function(msg){
-    time_stamp = msg;
-    is_playing = false;
-    console.log('pause at ' + time_stamp);
-    socket.broadcast.emit('pause', time_stamp);
-  });
-  socket.on('move', function(msg){
-    time_stamp = msg;
-    console.log('move to ' + time_stamp);
-    socket.broadcast.emit('move', time_stamp);
-  });
+	// socket.emit('id', id);
+	socket.on('onLoad', function(){
+		socket.emit('id', id);
+		if(isPlaying) {
+			socket.emit('play', timeStamp);
+		} else {
+			socket.emit('pause', timeStamp);
+		}
+	});
+	socket.on('id', function(msg){
+		id = msg;
+		console.log('play at ' + timeStamp);
+		socket.broadcast.emit('id', id);
+	});
+	socket.on('play', function(msg){
+		if(!isPlaying || Math.abs(timeStamp - msg) > threshold) {
+			timeStamp = msg;
+			isPlaying = true;
+			console.log('play at ' + timeStamp);
+			socket.broadcast.emit('play', timeStamp);
+		}
+	});
+	socket.on('pause', function(msg){
+		if(isPlaying || Math.abs(timeStamp - msg) > threshold) {
+			timeStamp = msg;
+			isPlaying = false;
+			console.log('pause at ' + timeStamp);
+			socket.broadcast.emit('pause', timeStamp);
+		}
+	});
+	socket.on('move', function(msg){
+		if(Math.abs(timeStamp - msg) > threshold) {
+			timeStamp = msg;
+			console.log('move to ' + timeStamp);
+			socket.broadcast.emit('move', timeStamp);
+		}
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
