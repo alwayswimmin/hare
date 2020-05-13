5caa313f70b3f3398392d649ccbd0950288d1987
Samuel Hsiang <shsiang@shsiang-mbp.local>
2020-05-13 00:34:28 -0700

    names and fixed logic

--- index.js
+++ index.js
@@ -25,7 +25,30 @@ app.get('/room/:roomId', function(req, res) {
     });
 });
 
-var state = new Array();
+var state = new Map();
+var timestamp = new Map();
+var names = new Map();
+
+function getAdjustedState(room) {
+    const snapshot = state.get(room);
+    if (!snapshot || snapshot.playerState != 1) return snapshot;
+    const offset = snapshot.playbackRate * ((new Date()).getTime() -
+        timestamp.get(room).getTime()) / 1000.0;
+    return {
+        'videoId': snapshot.videoId,
+        'playerState': snapshot.playerState,
+        'playbackRate': snapshot.playbackRate,
+        'currentTime': snapshot.currentTime + offset
+    };
+}
+
+function getValues(map) {
+    var array = new Array();
+    for (let [key, value] of map) {
+        array.push(value);
+    }
+    return array;
+}
 
 io.on('connection', function(socket) {
     console.log(socket.id);
@@ -34,13 +57,27 @@ io.on('connection', function(socket) {
         console.log(msg);
         room = msg;
         socket.join(room);
-        socket.emit('stateUpdate', state[room]);
+        socket.emit('stateUpdate', getAdjustedState(room));
+        var roomNames = names.get(room);
+        if (!roomNames) {
+            roomNames = new Map();
+            names.set(room, roomNames);
+        }
+        roomNames.set(socket.id, 'Anonymous');
+        io.to(room).emit('namesUpdate', getValues(roomNames));
     });
     socket.on('stateUpdate', function(msg) {
         console.log(socket.id + ' stateUpdate:');
         console.log(msg);
-        state[room] = msg;
-        socket.to(room).emit('stateUpdate', state[room]);
+        state.set(room, msg);
+        timestamp.set(room, new Date());
+        socket.to(room).emit('stateUpdate', msg);
+    });
+    socket.on('nameUpdate', function(msg) {
+        console.log(socket.id + ' nameUpdate: ' + msg);
+        var roomNames = names.get(room);
+        roomNames.set(socket.id, msg);
+        io.to(room).emit('namesUpdate', getValues(roomNames));
     });
 });
 
